c65a21dcd28ae3b3700359ab18f96a6fcf796484
baptiste <baptiste454@gmail.com>
2019-12-10 23:40:06 -0500

    add lighting function

--- include/light.hpp
+++ include/light.hpp
@@ -47,6 +47,19 @@ namespace light {
    */    
   bool operator!=(const PointLight& first, const PointLight& second);
 
+  /* \fn color::Color lighting(std::shared_ptr<geo::Shape> object, const PointLight light, const math::Tuple& position, const math::Tuple& eye_vector, const math::Tuple& normal_vector, const bool in_shadow)
+   * Calculate the Color of the Shape's Material at the given position
+   * \param object lit Shape
+   * \param light the scene's light
+   * \param position the position of the point on the object
+   * \param eye_vector vector surface --> eye
+   * \param normal_vector object's surface normal
+   * \param in_shadow, true if the point is in the shadow
+   * \return the Color at this point
+   */  
+  color::Color lighting(std::shared_ptr<geo::Shape> object, const PointLight light, const math::Tuple& position,
+			const math::Tuple& eye_vector, const math::Tuple& normal_vector, const bool in_shadow);
+
 }
 
 #endif
