1133ba18dbc8e566119b32a83ff6a547a8676147
baptiste <baptiste454@gmail.com>
2019-12-10 23:40:06 -0500

    add lighting function

--- src/light.cpp
+++ src/light.cpp
@@ -1,5 +1,7 @@
 #include <memory>
+#include <cmath>
 
+#include "tuple.hpp"
 #include "light.hpp"
 #include "geo.hpp"
 #include "color.hpp"
@@ -19,57 +21,58 @@ namespace light {
     return !(first == second);
   }    
 
-  // color::Color lighting(std::shared_ptr<geo::Shape>, const PointLight light, const math::Tuple& position,
-  // 			const math::Tuple& eye_vector, const math::Tuple& normal_vector, const bool in_shadow) {
+  color::Color lighting(std::shared_ptr<geo::Shape> object, const PointLight light, const math::Tuple& position,
+  			const math::Tuple& eye_vector, const math::Tuple& normal_vector, const bool in_shadow) {
 
-  //   // Use material or pattern as color    
-  //   auto color = color::Color();
-  //   if (object->material.pattern != std::nullopt)
-  //     color = object->material.pattern->stripe_at(position);
-  //   else
-  //     color = object->material.color;
+    // Use material or pattern as color    
+    auto color = color::Color();
+    // if (object->material.pattern != std::nullopt)
+    //   color = object->material.pattern->stripe_at(position);
+    // else
+    //   color = object->material.color;
+    color = object->material.color; // TO REPLACE WITH ABOVE WHEN PATTERN
     
-  //   // Combine the surface color with the light
-  //   color::Color effective_color = color * light.intensity;
+    // Combine the surface color with the light
+    color::Color effective_color = color * light.intensity;
 
-  //   // Direction to the light source
-  //   auto light_vector = math::normalize(light.position - position);
+    // Direction to the light source
+    auto light_vector = math::normalize(light.position - position);
 
-  //   // Ambient contribution
-  //   color::Color ambient = effective_color * object->material.ambient;
+    // Ambient contribution
+    color::Color ambient = effective_color * object->material.ambient;
 
-  //   // When the point is in shadow, the color is simply the ambient
-  //   if (in_shadow)
-  //     return ambient;
+    // When the point is in shadow, the color is simply the ambient
+    if (in_shadow)
+      return ambient;
 
-  //   color::Color diffuse;
-  //   color::Color specular;
-  //   // light_dot_nornal represents the cosine of the angle between the light vector and the normal vector
-  //   // A negative number means the light is on the other side of the surface
-  //   auto light_dot_normal = math::dot(light_vector, normal_vector);
-  //   if (light_dot_normal < 0) {
-  //     diffuse = color::Color(0, 0, 0);
-  //     specular = color::Color(0, 0, 0);
-  //   } else {
-  //     // Difuse contribution
-  //     diffuse = effective_color * object->material.diffuse * light_dot_normal;
+    color::Color diffuse;
+    color::Color specular;
+    // light_dot_nornal represents the cosine of the angle between the light vector and the normal vector
+    // A negative number means the light is on the other side of the surface
+    auto light_dot_normal = math::dot(light_vector, normal_vector);
+    if (light_dot_normal < 0) {
+      diffuse = color::Color(0, 0, 0);
+      specular = color::Color(0, 0, 0);
+    } else {
+      // Difuse contribution
+      diffuse = effective_color * object->material.diffuse * light_dot_normal;
 
-  //     // reflect_dot_eye represents the cosine of the angle between the reflection vector and the eye vector
-  //     // A negative number means the light reflects away from the eye
-  //     auto reflect_vector = geo::reflect(-light_vector, normal_vector);
-  //     double reflect_dot_eye = math::dot(reflect_vector, eye_vector);
+      // reflect_dot_eye represents the cosine of the angle between the reflection vector and the eye vector
+      // A negative number means the light reflects away from the eye
+      auto reflect_vector = geo::reflect(-light_vector, normal_vector);
+      double reflect_dot_eye = math::dot(reflect_vector, eye_vector);
 
-  //     if (reflect_dot_eye < 0)
-  // 	specular = color::Color(0, 0, 0);
-  //     else {
-  //       // Specular contribution
-  //       double factor = pow(reflect_dot_eye, double{object->material.shininess});
-  // 	specular = light.intensity * object->material.specular * factor;
-  //     }
-  //   }
+      if (reflect_dot_eye < 0)
+  	specular = color::Color(0, 0, 0);
+      else {
+        // Specular contribution
+        double factor = pow(reflect_dot_eye, double{object->material.shininess});
+  	specular = light.intensity * object->material.specular * factor;
+      }
+    }
   
-  //   // Add the three contributions together to get the final shading
-  //   return ambient + diffuse + specular;
-  // }  
+    // Add the three contributions together to get the final shading
+    return ambient + diffuse + specular;
+  }  
   
 }
--- tests/tests_light.cpp
+++ tests/tests_light.cpp
@@ -1,13 +1,18 @@
 
+#include <memory>
+#include <cmath>
+
 #include <gtest/gtest.h>
 
 #include "ray.hpp"
 #include "geo.hpp"
 #include "tuple.hpp"
 #include "light.hpp"
+#include "color.hpp"
+#include "material.hpp"
 
 
-TEST(PointLightTest, Constructor) {
+TEST(LightTest, PointLightConstructor) {
 
   // A point light has a position and intensity
   auto position = math::Point(0.0, 0.0, 0.0);
@@ -18,7 +23,7 @@ TEST(PointLightTest, Constructor) {
   EXPECT_EQ(light.intensity, intensity);
 }
 
-TEST(PointLightTest, Equality) {
+TEST(LightTest, PointLightEquality) {
 
   auto position = math::Point(0.0, 0.0, 0.0);
   auto intensity = color::Color(1, 1, 1);
@@ -35,5 +40,60 @@ TEST(PointLightTest, Equality) {
   EXPECT_EQ(light1, light3);
   EXPECT_NE(light1, light2);
   EXPECT_NE(light3, light2);
+}
+
+TEST(LightTest, LightingFunction) {
+
+  auto sphere = std::make_shared<geo::Sphere>();
+  auto mat = material::Material();
+  sphere->material = mat;
+  auto position = math::Point(0.0, 0.0, 0.0);
+
+  // Lighting with the eye between the light and surface
+  // --> intensity = 0.1 (ambient) + 0.9 (diffuse) + 0.9 (specular)
+  auto eyev = math::Vector(0.0, 0.0, -1.0);
+  auto normalv = math::Vector(0.0, 0.0, -1.0);
+  auto light = light::PointLight(math::Point(0.0, 0.0, -10.0), color::Color(1, 1, 1));
+
+  auto result = light::lighting(sphere, light, position, eyev, normalv, false);
+  EXPECT_EQ(result, color::Color(1.9, 1.9, 1.9));
+
+  // Lighting with the eye between light and surface, eye offset 45 degrees
+  // --> intensity = 0.1 (ambient) + 0.9 (diffuse) + 0.0 (specular)  
+  eyev = math::Vector(0.0, sqrt(2)/2, sqrt(2)/2);
+  normalv = math::Vector(0.0, 0.0, -1.0);
+  light = light::PointLight(math::Point(0.0, 0.0, -10.0), color::Color(1, 1, 1));
+  result = light::lighting(sphere, light, position, eyev, normalv, false);
+  EXPECT_EQ(result, color::Color(1.0, 1.0, 1.0));
+
+  // Lighting with eye opposite surface, light offset 45 degrees
+  eyev = math::Vector(0.0, 0.0, -1.0);
+  normalv = math::Vector(0.0, 0.0, -1.0);
+  light = light::PointLight(math::Point(0.0, 10.0, -10.0), color::Color(1, 1, 1));
+  result = light::lighting(sphere, light, position, eyev, normalv, false);
+  EXPECT_EQ(result, color::Color(0.7364, 0.7364, 0.7364));
+
+  // Lighting with eye in the path of the reflection vector
+  eyev = math::Vector(0.0, -sqrt(2)/2.0, -sqrt(2)/2.0);
+  normalv = math::Vector(0.0, 0.0, -1.0);
+  light = light::PointLight(math::Point(0.0, 10.0, -10.0), color::Color(1, 1, 1));
+  result = light::lighting(sphere, light, position, eyev, normalv, false);
+  EXPECT_EQ(result, color::Color(1.6364, 1.6364, 1.6364));
+
+  // Lighting with the light behind the surface
+  // total intensity should = ambient
+  eyev = math::Vector(0.0, 0.0, -1.0);
+  normalv = math::Vector(0.0, 0.0, -1.0);
+  light = light::PointLight(math::Point(0.0, 0.0, 10.0), color::Color(1, 1, 1));
+  result = light::lighting(sphere, light, position, eyev, normalv, false);
+  EXPECT_EQ(result, color::Color(0.1, 0.1, 0.1));
+  
+  // Lighting with the surface in shadow
+  eyev = math::Vector(0, 0, -1);
+  normalv = math::Vector(0, 0, -1);
+  light = light::PointLight(math::Point(0, 0, -10), color::Color(1, 1, 1));
+  bool in_shadow = true;
+  result = light::lighting(sphere, light, position, eyev, normalv, in_shadow);
+  EXPECT_EQ(result, color::Color(0.1, 0.1, 0.1));
 }
 
